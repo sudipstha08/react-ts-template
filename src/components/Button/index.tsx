3a819c938725afab092eaca89956626d97e1c8f0
Sudip Shrestha <sudipstha08@gmail.com>
2021-05-24 21:37:02 +0545

    :zap: feat: add component and container

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from 'react'
+
+interface IProps {
+  label: string;
+}
+
+const Button: FC<IProps> = ({ label }) => {
+  return (
+    <button>
+      label
+    </button>
+  )
+}
+
+export { Button }
