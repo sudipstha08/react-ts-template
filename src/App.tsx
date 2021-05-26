537da315e55e5ffcc486b06e24d0ffd1aa2f36b1
Sudip Shrestha <sudipstha08@gmail.com>
2021-05-26 13:05:17 +0545

    :book: docs: update readme

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
 import React from 'react'
-import logo from './logo.svg'
+import { Router, Switch, Route } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import HomePage from './containers/HomePage'
 import './App.css'
+
+const queryClient = new QueryClient()
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+          </Switch>
+        </Router>
+      </QueryClientProvider>
     </div>
   )
 }
