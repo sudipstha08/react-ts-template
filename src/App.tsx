dbe434c2522e367d59d2a83898cde804d6f07276
sudipstha08 <sudipstha08@gmail.com>
2024-06-12 20:15:19 +0545

    feat: update vscode configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { QueryClientProvider } from '@tanstack/react-query'
-import { Button } from '@/components'
 import { queryClient } from '@/lib'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from '@/components'
 
 function App() {
   return (
