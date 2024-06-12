5696434da81a0e8a203953ea8f946919d0a4ab7c
sudipstha08 <sudipstha08@gmail.com>
2024-06-12 20:08:10 +0545

    fix: index page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
 import { QueryClientProvider } from '@tanstack/react-query'
 import { Button } from '@/components'
 import { queryClient } from '@/lib'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <h1>HomePage</h1>
-      <Button label="Click Me" />
+      <main
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <h1>HomePage</h1>
+        <img src={viteLogo} className="logo" alt="Vite logo" height={100} />
+        <a href="https://react.dev" target="_blank">
+          <img
+            src={reactLogo}
+            className="logo react"
+            alt="React logo"
+            height={300}
+          />
+        </a>
+        <Button label="Click Me" />
+      </main>
     </QueryClientProvider>
   )
 }
